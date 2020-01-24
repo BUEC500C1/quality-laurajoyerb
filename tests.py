7340dcef10e8324236d8598a25b54d93a8d10a56
Laura Joy Erb <laurajoy@bu.edu>
2020-01-24 14:33:58 -0500

    added test cases

--- tests.py
+++ tests.py
@@ -1,7 +1,25 @@
 from arabic2roman import arabic2roman
 
-def test_one():
+def test_normal():
     assert arabic2roman(34) == 'XXXIV'
-#
-# def test_two():
-#     assert arabic2roman() == 3
+
+def test_small():
+    assert arabic2roman(2) == 'II'
+
+def test_large():
+    assert arabic2roman(3964) == 'MMMCMLXIV'
+
+def test_out_of_range():
+    assert arabic2roman(5678) == ''
+
+def test_string():
+    assert arabic2roman('hello') == ''
+
+def test_mixed():
+    assert arabic2roman('hel123lo') == ''
+
+def test_float():
+    assert arabic2roman(3.14) == ''
+
+def test_start_num():
+    assert arabic2roman('123hello') == ''
