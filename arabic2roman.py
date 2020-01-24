712af70572adcf1fa29f66c5fbf07b2f4bae58ef
Laura Joy Erb <laurajoy@bu.edu>
2020-01-24 17:07:15 -0500

    added linter improvements

--- arabic2roman.py
+++ arabic2roman.py
@@ -2,11 +2,12 @@
 # EC500: Assignment 1
 # Program to convert arabic numbers to roman numerals
 # References:
-    # https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
+# https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
+
 
 def arabic2roman(arabic):
     if type(arabic) != int:
-        print("Invalid arabic numeral. Please enter an integer value (1, 2, 3, etc)")
+        print("Invalid arabic numeral, not an integer.")
         return ''
 
     if arabic > 3999:
@@ -14,7 +15,12 @@ def arabic2roman(arabic):
         return ''
 
     decimal = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
-    romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    romans = [
+        "I", "IV", "V",
+        "IX", "X", "XL",
+        "L", "XC", "C",
+        "CD", "D", "CM", "M"
+        ]
 
     i = len(decimal)
     i -= 1
