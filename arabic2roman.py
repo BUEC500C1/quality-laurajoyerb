6fcc066a092cffa3402944cb12f83934cfea3ffc
Laura Joy Erb <laurajoy@bu.edu>
2020-01-24 14:33:52 -0500

    changed error checking for int

--- arabic2roman.py
+++ arabic2roman.py
@@ -3,16 +3,15 @@
 # Program to convert arabic numbers to roman numerals
 # References:
     # https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
-    # https://www.101computing.net/number-only/
 
-def arabic2roman(arabic_num):
-    try:
-        arabic = int(arabic_num)
-    except ValueError:
+def arabic2roman(arabic):
+    if type(arabic) != int:
         print("Invalid arabic numeral. Please enter an integer value (1, 2, 3, etc)")
         return ''
 
-    # TODO: add checking for range of integers
+    if arabic > 3999:
+        print("Please enter a numeral less than 4000")
+        return ''
 
     decimal = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
     romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
